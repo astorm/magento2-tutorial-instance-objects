a3e1d336a7a4bbb2edf1b2ddb5e6f61e468ed460
Alan Storm <astorm@alanstorm.com>
2015-08-30 14:40:23 -0700

    Pre tag

--- app/code/Pulsestorm/TutorialInstanceObjects/Command/Testbed.php
+++ app/code/Pulsestorm/TutorialInstanceObjects/Command/Testbed.php
@@ -5,15 +5,32 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Pulsestorm\TutorialInstanceObjects\Model\ExampleFactory;
+use \Magento\Framework\ObjectManagerInterface;
 
 class Testbed extends Command
 {
-    protected $exampleFactory;
-    public function __construct(ExampleFactory $example)
-    {
-        $this->exampleFactory = $example;
-        return parent::__construct();
-    }
+    // protected $exampleFactory;
+    // public function __construct(ExampleFactory $manager)
+    // {
+    //     $this->exampleFactory = $example;
+    //     $this->manager = $manager;
+    //     return parent::__construct();
+    // }
+    
+    // protected $manager;            
+    // public function __construct(ObjectManagerInterface $manager)
+    // {
+    //     $this->exampleFactory = $example;
+    //     $this->manager = $manager;
+    //     return parent::__construct();
+    // }
+    
+    // protected $pageFactory;
+    // public function __construct(\Magento\Cms\Model\PageFactory $pageFactory )
+    // {
+    //     $this->pageFactory = $pageFactory;
+    //     parent::__construct();
+    // }
     
     protected function configure()
     {
@@ -24,10 +41,28 @@ class Testbed extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln("Hello World"); 
-        $output->writeln(get_class($this->exampleFactory)); 
+        $output->writeln("You've installed Pulsestorm_TutorialInstanceObjects");
+        // $page = $this->pageFactory->create();
+        // foreach($page->getCollection() as $item)
+        // {
+        //     $output->writeln($item->getId() . '::' . $item->getTitle());
+        // }
+        //         
+        // $page = $this->pageFactory->create()->load(1);        
+        // var_dump($page->getData());
+                
+        // $example = $this->exampleFactory->create();
+        // $output->writeln(
+        //     "You just used a"                . "\n\n    " .
+        //     get_class($this->exampleFactory) . "\n\n"     . 
+        //     "to create a \n\n    "           . 
+        //     get_class($example)              . "\n"); 
         
-        $example = $this->exampleFactory->create();
-        $output->writeln(get_class($example)); 
+        
+        // $object = 
+        //     $this->manager->create('Pulsestorm\TutorialInstanceObjects\Model\Example');
+        // $object = 
+        //     $this->manager->get('Pulsestorm\TutorialInstanceObjects\Model\Example');
+
     }
 } 
